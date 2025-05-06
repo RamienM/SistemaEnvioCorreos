1c1069aaeeb888d61a6c273cae8cf77cb57fadc1
Rubén Ramis Martínez <rubenduque12345@gmail.com>
2025-05-06 15:56:55 +0200

    Deleted not used import

--- src/main/java/org/secr/sistemaenviocorreos/service/EmailConsumer.java
+++ src/main/java/org/secr/sistemaenviocorreos/service/EmailConsumer.java
@@ -14,7 +14,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.MailAuthenticationException;
 import org.springframework.mail.MailSendException;
-import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.mail.javamail.MimeMessageHelper;
